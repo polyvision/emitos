67c3f1ac746a9816952d96eea63969180327d5bc
Alexander Bierbrauer <abierbrauer@polyvision.org>
2013-02-18 15:16:06 +0100

    cycling for market(ing) call

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,25 @@
 module ApplicationHelper
 
+  def boolean_icon_sprite(state)
+    if state
+      return '<i class="icon-ok"></i>'
+    end
+    return '<i class="icon-remove"></i>'
+  end
+
+  def flash_class(level)
+    case level
+      when :notice then
+        "alert alert-info"
+      when :success then
+        "alert alert-success"
+      when :error then
+        "alert alert-error"
+      when :alert then
+        "alert alert-error"
+    end
+  end
+
   def display_base_errors resource
     return '' if (resource.errors.empty?) or (resource.errors[:base].empty?)
     messages = resource.errors[:base].map { |msg| content_tag(:p, msg) }.join
@@ -12,28 +32,28 @@ module ApplicationHelper
     html.html_safe
   end
 
-  def button_link(label_txt,controller_stuff,icon_stuff = false,class_xtra = false)
+  def button_link(label_txt, controller_stuff, icon_stuff = false, class_xtra = false)
     if icon_stuff == false
       label = label_txt
     else
       label = "<i class=\"#{icon_stuff}\"></i> #{label_txt}"
     end
-    
+
     class_str = "btn btn-primary"
     if class_xtra
       class_str << " " << class_xtra
     end
 
-    return link_to(raw(label),controller_stuff,{:class => class_str})
+    return link_to(raw(label), controller_stuff, {:class => class_str})
   end
 
-  def button_link_confirm(label_txt,controller_stuff,icon_stuff = false,confirm_text = false)
+  def button_link_confirm(label_txt, controller_stuff, icon_stuff = false, confirm_text = false)
     if icon_stuff == false
       label = label_txt
     else
       label = "<i class=\"#{icon_stuff}\"></i> #{label_txt}"
     end
-    
-    return link_to(raw(label),controller_stuff,{:class => 'btn btn-primary',:confirm => confirm_text})
+
+    return link_to(raw(label), controller_stuff, {:class => 'btn btn-primary', :confirm => confirm_text})
   end
 end
--- app/models/marketing_call.rb
+++ app/models/marketing_call.rb
@@ -1,16 +1,23 @@
 class MarketingCall < ActiveRecord::Base
-  attr_accessible :sound_file_id, :name, :play_at, :use_matrix,:day_matrix, :minutes_pro_call
+  attr_accessible :sound_file_id, :name, :play_at, :use_matrix, :day_matrix, :minutes_pro_call
   belongs_to :sound_file
 
   def update_matrix(params)
-      json_data = Array.new
-       for i in 0..6 do
-         if params[:matrix][i.to_s] == "on"
-           json_data[i] = true
-         else
-           json_data[i] = false
-         end
-       end
+    if !params[:matrix]
+      self.day_matrix = Array.new.to_json
+      self.save
+      return
+    end
+
+
+    json_data = Array.new
+    for i in 0..6 do
+      if params[:matrix][i.to_s] == "on"
+        json_data[i] = true
+      else
+        json_data[i] = false
+      end
+    end
 
     self.day_matrix = json_data.to_json
     self.save
@@ -18,17 +25,35 @@ class MarketingCall < ActiveRecord::Base
 
   def check_matrix(index)
     if self.day_matrix
-    	json_data = JSON::parse(self.day_matrix)
-    	return json_data[index]
+      json_data = JSON::parse(self.day_matrix)
+      return json_data[index]
     else
-     return false
+      return false
     end
   end
 
+  # helper method to check if the call should be played today, the time doesn matter.. just the day matrix
+  def play_today?
+    if self.use_matrix == false #der tag ist egal
+      return true
+    end
+
+    if !self.day_matrix #tag ist nicht egal, ist gibt aber keinen aktiven tag...
+      return false
+    end
+
+    cweekday = Time.now.to_date.cwday
+
+    if self.check_matrix(cweekday)
+      return true
+    end
+    return false
+  end
+
   def matrix_str
-     if !self.day_matrix
-	return ""
-     end
+    if !self.day_matrix
+      return ""
+    end
     json_data = JSON::parse(self.day_matrix)
 
     days = ""
@@ -61,5 +86,79 @@ class MarketingCall < ActiveRecord::Base
       days << "Sa,"
     end
     return days
+  end
+
+  def play_sound_on_psd
+    begin
+      TCPSocket.open(Setting.get_val("ESD_HOST"), Setting.get_val("PSD_PORT").to_i) { |s|
+        s.send "PLAY #{self.sound_file.file.current_path}#", 0
+      }
+    rescue Exception => ex
+      puts "#{self.id} MarketingCall.play_sound_on_psd error: #{ex.message}"
+    end
+  end
+
+  # returns the play at converted to the current day
+  def current_play_at
+    return Time.zone.parse("#{Time.now.to_date.year}-#{Time.now.to_date.month}-#{Time.now.to_date.day} #{self.play_at.hour}:#{self.play_at.min}")
+  end
+
+  def already_played_today?
+    lpa = self.last_played_at
+    if !lpa
+      return false
+    end
+
+    ct = Time.now
+    if lpa.year == ct.year && lpa.month == ct.month && lpa.day == ct.day
+      return true
+    end
+
+    return false
+  end
+
+  def self.cycle
+
+    MarketingCall.all.each do |mc|
+      if (mc.play_today?)
+        if mc.minutes_pro_call != 0
+          MarketingCall::cycle_non_time_based(mc)
+        else # it's clock time based
+          MarketingCall::cycle_time_based(mc)
+        end # minutes pro call != 0
+      else
+        puts "MarketingCall.cycle: play_today? NO ##{mc.id} #{mc.name}"
+      end # check play_today?
+    end # marketing call each
+  end
+
+  def self.cycle_time_based(mc)
+    puts "MarketingCall:cycle_time_based  ##{mc.id} #{mc.name}"
+    if mc.already_played_today? == false
+      if mc.current_play_at.to_i <= Time.now.to_i
+        mc.play_sound_on_psd
+        mc.last_played_at= Time.now
+        mc.save
+        puts "MarketingCall:cycle_time_based: playing call ##{mc.id} #{mc.name}"
+      end
+    else
+      puts "MarketingCall:cycle_time_based: already_played_today? YES  ##{mc.id} #{mc.name}"
+    end # mc.already_played_today? == false
+  end
+
+  def self.cycle_non_time_based(mc)
+    puts "MarketingCall:cycle_non_time_based  ##{mc.id} #{mc.name}"
+    last_played_at_cal = (mc.minutes_pro_call * 60) # default value
+    if mc.last_played_at && mc.last_played_at.to_s.length > 1 # in the first run, it's null
+      last_played_at_cal = Time.now.to_i - mc.last_played_at.to_i
+    end
+
+    if last_played_at_cal >= (mc.minutes_pro_call * 60)
+
+      mc.last_played_at = Time.now
+      mc.save
+      mc.play_sound_on_psd
+      puts "MarketingCall:cycle_non_time_based: playing call ##{mc.id} #{mc.name}"
+    end
   end
 end
