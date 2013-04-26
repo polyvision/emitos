41208ccc900e72f2138b82b3786b5f7c6d65da4a
Alexander Bierbrauer <abierbrauer@polyvision.org>
2013-04-26 10:41:52 +0200

    fixes

--- app/models/sound_file.rb
+++ app/models/sound_file.rb
@@ -1,15 +1,21 @@
 class SoundFile < ActiveRecord::Base
   attr_accessible :file, :sound_group_id
 
-  mount_uploader :file,SoundFileUploader
+  mount_uploader :file, SoundFileUploader
 
   belongs_to :sound_group
 
   def filename
-  	return File.basename(self.file.current_path)
+    begin
+      return File.basename(self.file.current_path)
+    rescue Exception => ex
+    end
   end
 
   def to_s
-  	return "#{self.sound_group.name} - #{self.filename}"
+    begin
+      return "#{self.sound_group.name} - #{self.filename}"
+    rescue Exception => ex
+    end
   end
 end
